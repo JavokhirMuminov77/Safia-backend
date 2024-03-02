654e2bd860d60cf80e7c1cea53dc672adaf031da
javokhirMuminov <javokhirmuminovb@gmail.com>
2024-03-02 16:59:35 +0900

    feat:creat restaurant reladet get and post endpounts

--- src/routerAdmin.ts
+++ src/routerAdmin.ts
@@ -3,9 +3,21 @@ const routerAdmin = express.Router();
 import memberController from './controllers/member.controller';
 import restaurantController from "./controllers/restaurant.controller";
 
-
+/**Restaurant */ 
 routerAdmin.get("/", restaurantController.goHome );
-routerAdmin.get("/login", restaurantController.getLogin);
-routerAdmin.get("/signup", restaurantController.getSignup);
 
+
+routerAdmin
+.get("/login", restaurantController.getLogin)
+.post("/login", restaurantController.processLogin);
+
+routerAdmin
+.get("/signup", restaurantController.getSignup)
+.post("/signup", restaurantController.processSignup);
+
+
+
+/*Prodect*/
+
+/**User*/
 export default routerAdmin;
