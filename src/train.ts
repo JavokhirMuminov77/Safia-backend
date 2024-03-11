7e008654f0bb6a911794fd52eb18d7795312163b
javokhirMuminov <javokhirmuminovb@gmail.com>
2024-03-11 13:51:17 +0900

    deat: develop admin frontend pages and integrate header & footer

--- src/train.ts
+++ src/train.ts
@@ -1,14 +1,9 @@
-/*I - task*/
+// Task - J
+function findLongestWord(str: string): string {
+  let result = str
+    .split(" ")
+    .reduce((acc, curr) => (acc.length >= curr.length ? acc : curr));
+  return result;
+}
 
-/*Buyeda matvh proportisi orqali arrayda string qiymat bor yuqliguini aniqlab oldik va join proportisi yordamida uni bush joyga tenglab oldik*/
-
-function getDigits(input: string): string {
-   const result = input.match(/\d+/g)?.join("");
-
-   return result ? result : "";
-   /*buyerda  shart operatori agar result teng bolsa result bolmasa  "" qaytar dedit*/
- }
-
- console.log(getDigits("m14i1t"));
-/* .c  dssmkksd dfskmkmaskmx dwdew .s'/.d's;d's ddsd//s';.ds;'ldf.s ;sd
-
+  console.log(findLongestWord("I come from Uzbekistan"));
