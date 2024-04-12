8a533990801259605399dfe37f9962ab6eb08d57
javokhirMuminov <javokhirmuminovb@gmail.com>
2024-04-12 18:26:46 +0900

    feat: develop verify user credentials

--- src/controllers/member.controller.ts
+++ src/controllers/member.controller.ts
@@ -2,7 +2,7 @@ import{T} from "../libs/types/common";
 import { Request, Response } from "express";
 import MemberService from "../modules/Member.service";
 import { LoginInput, Member, MemberInput } from "../libs/types/member";
-import Errors, { HttpmCode } from "../libs/Errors";
+import Errors, { HttpmCode, Message } from "../libs/Errors";
 import AuthService from "../modules/Auth.service";
 import { AUTH_TIMER } from "../libs/config";
 
@@ -61,5 +61,26 @@ memberController.login = async (req: Request, res: Response) => {
 
 
 
+
+memberController.verifyAuth = async (req: Request, res: Response) => {
+
+  try {
+    let member = null;
+    const token =req.cookies["accessToken"];
+    if(token) member = await authService.checkAuth(token);
+
+    if(!member)
+     throw new Errors(HttpmCode.UNAUTHORIZED, Message.NOT_AUTHENTICATED);
+
+    console.log("member:", member);
+    res.status(HttpmCode.OK).json({member: member});
+
+  }catch (err) {
+    console.log("Error, verifyAuth:", err);
+    if(err instanceof Errors) res.status(err.code).json(err);
+    else res.status(Errors.standard.code).json(Errors.standard);
+  }
+
+}
 
 export default memberController;
--- src/modules/Auth.service.ts
+++ src/modules/Auth.service.ts
@@ -5,8 +5,9 @@ import { Member } from "../libs/types/member";
 import jwt from "jsonwebtoken";
 
 class AuthService {
+  private readonly secretToken;
   constructor () {
-
+    this.secretToken = process.env.SECRET_TOKEN as string;
 
   }
 
@@ -27,6 +28,14 @@ class AuthService {
     })
 
   }
+
+  public async checkAuth(token: string): Promise<Member> {
+    const result: Member = (await jwt.verify(token, this.secretToken)) as Member;
+    console.log(`--[AUTH] memberNick: ${result.memberNick} ---`);
+    return result;
+  }
+
+
 }
 
 export default AuthService;
