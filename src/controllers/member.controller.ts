a531b7d06f97d649b50627074bb13e39e34949d3
javokhirMuminov <javokhirmuminovb@gmail.com>
2024-04-12 18:00:05 +0900

    feat:develop authentication cookes

--- src/controllers/member.controller.ts
+++ src/controllers/member.controller.ts
@@ -2,8 +2,9 @@ import{T} from "../libs/types/common";
 import { Request, Response } from "express";
 import MemberService from "../modules/Member.service";
 import { LoginInput, Member, MemberInput } from "../libs/types/member";
-import Errors from "../libs/Errors";
+import Errors, { HttpmCode } from "../libs/Errors";
 import AuthService from "../modules/Auth.service";
+import { AUTH_TIMER } from "../libs/config";
 
 const memberService = new MemberService();
 const authService = new AuthService ();
@@ -20,11 +21,15 @@ memberController.signup = async  (req: Request, res: Response) => {
     const input: MemberInput = req.body,
      result: Member = await memberService.signup(input);
      const token = await authService.createToken(result);
-     console.log("token", token);
 
-     //TODO: TOKENS
 
-    res.json({member: result});
+     res.cookie("accessToken", token,
+     { maxAge: AUTH_TIMER *3600 *1000,
+       httpOnly: false
+     });
+
+
+    res.status(HttpmCode.CREAT).json({member: result, accessToken: token});
   } catch (err) {
     console.log("Error, signup:", err);
     if(err instanceof Errors) res.status(err.code).json(err);
@@ -39,19 +44,14 @@ memberController.login = async (req: Request, res: Response) => {
     const input: LoginInput = req.body,
      result = await memberService.login(input),
      token = await authService.createToken(result);
-     console.log("token =>", token);
-    //TODO: TOKEN
 
-    console.log("result", result);
-
-
-
+    res.cookie("accessToken", token,
+    { maxAge: AUTH_TIMER *3600 *1000,
+      httpOnly: false
+    });
 
 
-
-
-
-    res.json({member: result});
+    res.status(HttpmCode.OK).json({member: result, accessToken: token});
   } catch (err) {
     console.log("Error, signup:", err);
     if(err instanceof Errors) res.status(err.code).json(err);
