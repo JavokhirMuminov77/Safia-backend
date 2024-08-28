bb7ad9686102c0b191a08bb9b7a84d0938f57c89
JavokhirMuminov77 <javokhirmuminovdev@gmail.com>
2024-08-28 22:46:34 +0900

    feat: prepare for production

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,9 @@
 import dotenv from 'dotenv';
-dotenv.config();
+dotenv.config( {
+    path: process.env.NODE_ENV === "production" ? ".env.production" : ".env",
+
+}
+);
 
 import mongoose from 'mongoose';
 import server from './app';
