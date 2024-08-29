eb38dd50aea0a796020e8ed57bff63d209b067b8
JavokhirMuminov77 <javokhirmuminovdev@gmail.com>
2024-08-30 01:03:44 +0900

    changes

--- process.config.js
+++ process.config.js
@@ -8,11 +8,11 @@ module.exports = {
       script: "./dist/server.js",
       watch: false,
       env_production: {
-       MONGO: 'mongodb+srv://javokhirmuminovdev:Javokhir7755@cluster0.mongodb.net/Obkeber-food?retryWrites=true&w=majority',
+       MONGO: 'mongodb+srv://javokhirmuminovdev:Javokhir7755cluster0.mongodb.net/Obkeber-food?retryWrites=true&w=majority',
         
       },
       env_development: {
-        NODE_ENV:'mongodb+srv://javokhirmuminovdev:Javokhir7755@cluster0.mongodb.net/Obkeber-food?retryWrites=true&w=majority',
+        NODE_ENV:'mongodb+srv://javokhirmuminovdev:Javokhir7755cluster0.mongodb.net/Obkeber-food?retryWrites=true&w=majority',
       },
       instances: 1,
       exec_mode: "cluster",
